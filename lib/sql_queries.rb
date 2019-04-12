e20a80ae9b8fa391874a243599b7555814920d76
Yonnick <yonnick00@gmail.com>
2019-04-12 13:01:28 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -40,7 +40,16 @@ HAVING sum(pledges.amount) >= projects.funding_goal
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
-"Write your SQL query Here"
+"
+SELECT users.name, sum(pledges.amount)
+FROM users
+INNER JOIN pledges
+ON users.id = pledges.user_id
+GROUP BY users.id
+ORDER BY pledges.aount, users.id
+
+
+"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
